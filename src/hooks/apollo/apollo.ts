a6a91f0e919ed76f68636e683345c77a827bcb14
haha <admin@qq.com>
2021-01-13 13:27:03 +0800

    重构 apollo

--- src/hooks/apollo/apollo.ts
+++ src/hooks/apollo/apollo.ts
@@ -1,14 +1,50 @@
 import apollo from '/@/lib/esm/apollo';
-import { ApolloClient } from '@apollo/client/core';
+// import { ApolloClient } from '@apollo/client/core';
 import { me } from '/@/hooks/apollo/gqlUser';
 import { useMessage } from '/@/hooks/web/useMessage';
 import { userStore } from '/@/store/modules/user';
-
+import { provide } from 'vue';
 const { createErrorModal } = useMessage();
 const Client = (): ApolloClient<any> => {
   return apollo.get();
 };
 // const apolloWSClient = apollo.getWS();
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { DefaultApolloClient } from '@vue/apollo-composable';
+import { ApolloLink, HttpLink } from '@apollo/client/core';
+// 与 API 的 HTTP 连接
+import { useQuery } from '@vue/apollo-composable';
+export function initApollo(): ApolloClient<any> | null {
+  const link = new HttpLink({
+    uri: 'https://owaf.io/api',
+  });
+
+  // split based on operation type
+  // REMOVE authLink FOR HTTPONLY_TOKEN
+  const middlewareLink = new ApolloLink((operation, forward) => {
+    if (forward === undefined) {
+      return null;
+    }
+    const token = localStorage.getItem('token');
+    operation.setContext({
+      headers: {
+        Authorization: token ? 'Bearer ' + token : '',
+      },
+    });
+    return forward(operation);
+  });
+  // 缓存实现
+  const cache = new InMemoryCache();
+
+  // 创建 apollo 客户端
+  const apolloClient = new ApolloClient({
+    link: middlewareLink.concat(link),
+    cache,
+    connectToDevTools: true,
+  });
+  provide(DefaultApolloClient, apolloClient);
+  return apolloClient;
+}
 
 export function useApollo(params: { mode: string; gql: any; variables?: any }): Promise<any> {
   return new Promise((resolve, reject) => {
@@ -17,7 +53,19 @@ export function useApollo(params: { mode: string; gql: any; variables?: any }): 
     let r: Promise<any>;
     switch (mode) {
       case 'query':
-        r = c.query({ query: gql, variables, fetchPolicy: 'network-only' });
+        const { result, onError } = useQuery(
+          gql,
+          () => variables,
+          () => ({
+            fetchPolicy: 'network-only',
+          })
+        );
+        onError((err) => {
+          console.log(err);
+          reject(err);
+        });
+        resolve(result);
+        // r = c.query({ query: gql, variables, fetchPolicy: 'network-only' });
         break;
       case 'mutate':
         r = c.mutate({ mutation: gql, variables });
@@ -25,19 +73,20 @@ export function useApollo(params: { mode: string; gql: any; variables?: any }): 
       default:
         reject('wrong params');
     }
+
     // @ts-ignore
-    r.then((res) => {
-      resolve(res);
-    }).catch((err) => {
-      if (err.message === 'Please sign in first!') {
-        userStore.loginOut(true);
-      }
-      createErrorModal({
-        title: '错误',
-        content: err.message,
-      });
-      reject(err);
-    });
+    // r.then((res) => {
+    //   resolve(res);
+    // }).catch((err) => {
+    //   if (err.message === 'Please sign in first!') {
+    //     userStore.loginOut(true);
+    //   }
+    //   createErrorModal({
+    //     title: '错误',
+    //     content: err.message,
+    //   });
+    //   reject(err);
+    // });
   });
 }
 
