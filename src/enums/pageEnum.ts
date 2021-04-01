d28f7623574450615d294b0d56ba03eaba4b8325
haha <admin@qq.com>
2021-04-02 02:22:04 +0800

    git

--- src/enums/pageEnum.ts
+++ src/enums/pageEnum.ts
@@ -1,14 +1,10 @@
 export enum PageEnum {
   // basic login path
   BASE_LOGIN = '/login',
-  // basic register path
-  BASE_REG = '/register',
   // basic home path
-  BASE_HOME = '/home/dashboard',
+  BASE_HOME = '/dashboard',
   // error page path
   ERROR_PAGE = '/exception',
-
-  SHARE_PAGE = '/general/s',
   // error log page path
   ERROR_LOG_PAGE = '/error-log/list',
 }
